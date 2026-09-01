b51b9235eac71af2f87968102d4a936b41fc0aac
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-01 01:45:34 -0500

    refactor(web): promote final responsive app entrypoint

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppV2 from './AppV2'
+import AppV3 from './AppV3'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppV2 />
+    <AppV3 />
   </StrictMode>,
 )
