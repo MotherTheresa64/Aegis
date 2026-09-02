dfff8a0905b478964aca2b1af9773326ba825452
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-01 19:28:46 -0500

    Align product UI with portfolio visual direction

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV2 from './AppV2'
 import './styles.css'
+import './showcase-design.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
