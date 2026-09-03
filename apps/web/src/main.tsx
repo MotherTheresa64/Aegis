405cb9622d9b99864f26c58efa41b402627afab2
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-03 02:17:49 -0500

    Add Aegis dark theme system

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV2 from './AppV2'
 import './styles.css'
+import './themes.css'
+import { initializeThemes } from './theme'
+
+initializeThemes()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
