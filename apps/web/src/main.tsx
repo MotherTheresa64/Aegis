eac5bab8425dc8931ef30868e2f4590119d68e52
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-09-03 03:32:57 -0500

    Reconcile hardened app entrypoint with current visual system

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './advanced.css'
 import './polish.css'
+import './showcase-design.css'
+import './themes.css'
+import { initializeThemes } from './theme'
 import AppV3 from './AppV3'
+
+initializeThemes()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
